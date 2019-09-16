e0950c8c109608266953d8fb68a49e618b39fa4f
Samet Koyuncu <sametkoyuncu@live.com>
2019-09-17 00:11:21 +0300

    Bazı kısımlar Türkçeleştirildi..

--- admin/includes/page-title-area.php
+++ admin/includes/page-title-area.php
@@ -4,21 +4,21 @@
                 <div class="row align-items-center">
                     <div class="col-sm-6">
                         <div class="breadcrumbs-area clearfix">
-                            <h4 class="page-title pull-left">Dashboard</h4>
+                            <h4 class="page-title pull-left">Anasayfa</h4>
                             <ul class="breadcrumbs pull-left">
-                                <li><a href="index.html">Home</a></li>
-                                <li><span>Dashboard</span></li>
+                                <li><a href="index.html">Kontrol Paneli</a></li>
+                                <li><span>Anasayfa</span></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-sm-6 clearfix">
                         <div class="user-profile pull-right">
                             <img class="avatar user-thumb" src="assets/images/author/avatar.png" alt="avatar">
-                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Kumkum Rai <i class="fa fa-angle-down"></i></h4>
+                            <h4 class="user-name dropdown-toggle" data-toggle="dropdown">Samet Koyuncu <i class="fa fa-angle-down"></i></h4>
                             <div class="dropdown-menu">
-                                <a class="dropdown-item" href="#">Message</a>
-                                <a class="dropdown-item" href="#">Settings</a>
-                                <a class="dropdown-item" href="#">Log Out</a>
+                                <a class="dropdown-item" href="#">Profili Göster</a>
+                                <a class="dropdown-item" href="#">Profili Düzenle</a>
+                                <a class="dropdown-item" href="#">Çıkış Yap</a>
                             </div>
                         </div>
                     </div>
--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -10,18 +10,94 @@
                     <nav>
                         <ul class="metismenu" id="menu">
                             <li class="active">
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>Kontrol Paneli</span></a>
                                 <ul class="collapse">
-                                    <li class="active"><a href="index.php">ICO dashboard</a></li>
-                                    <li class=""><a href="index.php">Pasif</a></li>
+                                    <li class="active"><a href="index.php">Anasayfa</a></li>
+                                    <li class=""><a href="index.php">Raporlar</a></li>
+                                </ul>
+                            </li>
+                            <hr style="border:0.5px solid darkslategrey">
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Site Bilgileri
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Genel Bilgiler</a></li>   
+                                <li class=""><a href="index.php">Meta Etiketleri</a></li>
                                 </ul>
                             </li>
                             <li>
-                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Pasif
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Bölümler
                                     </span></a>
                                 <ul class="collapse">
-                                <li class=""><a href="index.php">Pasif</a></li>
-                                <li class=""><a href="index.php">Pasif</a></li>
+                                <li class=""><a href="index.php">Üst Kısım</a></li>
+                                <li class=""><a href="index.php">Giriş</a></li>
+                                <li class=""><a href="index.php">Ürünler</a></li>
+                                <li class=""><a href="index.php">Galeri</a></li>
+                                <li class=""><a href="index.php">Blog</a></li>
+                                <li class=""><a href="index.php">İletişim</a></li>
+                                <li class=""><a href="index.php">Alt Kısım</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Tekil Sayfalar
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Çoklu Yazı Sayfası</a></li>   
+                                <li class=""><a href="index.php">Tekil Yazı Sayfası</a></li>
+                                <li class=""><a href="index.php">Sağ Sütun</a></li>
+                                </ul>
+                            </li>
+                            <!--<li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Sayfalar
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Tüm Sayfalar</a></li>
+                                <li class=""><a href="index.php">Yeni Ekle</a></li>
+                                </ul>
+                            </li>-->
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Yazılar
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Tüm Yazılar</a></li>
+                                <li class=""><a href="index.php">Yeni Ekle</a></li>
+                                <li class=""><a href="index.php">Kategoriler</a></li>
+                                <li class=""><a href="index.php">Etiketler</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Yorumlar
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Onaylananlar</a></li>
+                                <li class=""><a href="index.php">Onay Bekleyenler</a></li>
+                                <li class=""><a href="index.php">Çöp</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Bileşenler
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Tüm Bileşenler</a></li>
+                                <li class=""><a href="index.php">Yeni Ekle</a></li>
+                                </ul>
+                            </li>
+                            <hr style="border:0.5px solid darkslategrey">
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>E-Postalar
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Okunmamış</a></li>
+                                <li class=""><a href="index.php">Arşiv</a></li>
+                                <li class=""><a href="index.php">Çöp</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Aboneler
+                                    </span></a>
+                                <ul class="collapse">
+                                <li class=""><a href="index.php">Tüm Aboneler</a></li>
+                                <li class=""><a href="index.php">Yeni Ekle</a></li>
                                 </ul>
                             </li>
                         </ul>
